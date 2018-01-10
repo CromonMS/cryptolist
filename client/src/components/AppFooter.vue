980aaef1cde88d970e42fe7fc587dcb997d26d0d
CromonMS <e@cromon.co.uk>
2018-01-10 01:46:59 +0000

    Add links and donations to footer

--- client/src/components/AppFooter.vue
+++ client/src/components/AppFooter.vue
@@ -1,7 +1,22 @@
 <template>
   <footer class="footer is-sticky">
     <div class="container">
-      FOOTER
+      <div class="columns">
+        <div class="column is-two-thirds">
+          <h3 class="subtitle">Copyright &copy; 2018 Cromon</h3>
+          <p>Donate </p> 
+          <p>BTC: 1EDUSn47ddNQJ9wHX8WMAomN4McQYewcoN</p>
+          <p>BCH: 16EU5v9e79hVcPgPmrNQhY7cHJxQsABX35</p>
+          <p>ETH: 0xfb5337BfD765fDF7501ccF1650c14af4Bd0dd887</p>
+        </div>
+        <div class="column">
+          <h3 class="subtitle">Links</h3>
+          <p><a href="" title="">FAQ</a></p>
+          <p><a href="" title="">Terms</a></p>
+          <p><a href="" title="">Privacy Policy</a></p>
+          <p><a href="" title="">Contact</a></p>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
