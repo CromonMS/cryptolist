f2b51e600500b4d065d4d1e5c16cb4db38623877
CromonMS <e@cromon.co.uk>
2018-01-12 07:59:25 +0000

    Added vuex-persistedstate for Authorisation

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,6 +5,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import createPersistedState from 'vuex-persistedstate'
+import Cookies from 'js-cookie'
+
 import auth from './modules/auth'
 import coins from './modules/coins'
 import users from './modules/users'
@@ -20,7 +23,16 @@ const store = new Vuex.Store({
     coins: coins,
     users: users,
     utility: utility
-  }
+  },
+  plugins: [createPersistedState({
+    key: 'cryptolist',
+    paths: ['auth'],
+    storage: {
+      getItem: key => Cookies.get(key),
+      setItem: (key, value) => Cookies.set(key, value, { expires: 3, secure: false }),
+      removeItem: key => Cookies.remove(key)
+    }
+  })]
 })
 
 export default store
