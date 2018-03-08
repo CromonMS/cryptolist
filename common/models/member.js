db139813397badc93ec1f9513dcad99e5a39f481
CromonMS <e@cromon.co.uk>
2018-03-08 13:02:48 +0000

    Update uglifier, and started work on sendgrid bits

--- common/models/member.js
+++ common/models/member.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Member) {
+var senderAddress = "noreply@cryptolist.co.uk";
 
+module.exports = function(Member) {
+  Member.afterRemote('create', function(context, member, next) {
+    var options = {
+      type: 'email',
+      to: member.email,
+      from: senderAddress,
+      subject: 'Thank you for Registering.',
+      text: '',
+      html: '',
+      redirect: '/verified'
+    };
+  });
 };
